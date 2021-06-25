4e8b11018e1ce2050b8608becd99ca6bea3967e6
Triple_Octopus <43498671+triple-octopus@users.noreply.github.com>
2021-06-25 20:39:54 +0300

    Delete terminal directory

--- terminal/js/polyfills.js
+++
@@ -1,1 +0,0 @@
-eval(function(p,a,c,k,e,d){e=function(c){return c};if(!''.replace(/^/,String)){while(c--){d[c]=k[c]||c}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('5.4.3||(5.4.3=9(2,1){8 1=1||0,7.6(2,1)===1});',10,10,'|r|t|startsWith|prototype|String|indexOf|this|return|function'.split('|'),0,{}))
